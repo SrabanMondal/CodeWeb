e14188fd2045faa2d8a1aed9eb31e31c6562fd7c
Ashank007 <guptaashank38@gmail.com>
2025-03-03 22:34:59 +0530

    Backend Deleted

--- backend/Config/ConnectDb.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const ConnectDb = () => {
-	const uri = mongoose.connect(process.env.MONGO_URI, {
-		dbName: "Students"
-	}).then(() =>  console.log("DataBase Connected Successfully")).catch((error) => console.log(error));
-}
-export default ConnectDb;
--- backend/Utils/ApiError.js
+++
@@ -1,7 +0,0 @@
-class ApiError {
-  constructor(status = false, message) {
-    this.status = status;
-    this.message = message;
-  }
-}
-export default ApiError;
