af26724cc4cea3697bba9e20a1ef61e01684ae3f
Ashank007 <guptaashank38@gmail.com>
2025-03-17 18:41:38 +0530

    Added Mcq Section

--- backend/Models/Course.js
+++ backend/Models/Course.js
@@ -1,60 +1,52 @@
 import mongoose from "mongoose";
 
 const CourseScheme = new mongoose.Schema(
-	{
-		title: {
-			type: String,
-		},
-		courseid: {
-			type: String,
-		},
-		mcqsection: [
-			{
-				title: {
-					type: String,
-				},
-				description: {
-					type: String,
-				},
-				options: [
-					{
-						type: String,
-					},
-				],
-			},
-		],
-		easycodesection: [
-			{
-				title: {
-					type: String,
-				},
-				description: {
-					type: String,
-				},
-				testcases: [
-					{
-						type: String,
-					},
-				],
-			},
-		],
-		hardcodesection: [
-			{
-				title: {
-					type: String,
-				},
-				description: {
-					type: String,
-				},
-				testcases: [
-					{
-						type: String,
-					},
-				],
-			},
-		],
-	},
-	{ versionKey: false },
+  {
+    title: {
+      type: String,
+    },
+    courseid: {
+      type: String,
+    },
+    mcqsection: [
+      {
+        title: {
+          type: String,
+        },
+        description: {
+          type: String,
+        },
+        options: [
+          {
+            type: String,
+          },
+        ],
+      },
+    ],
+    section: [
+      {
+        title: {
+          type: String,
+        },
+        questions: [
+          {
+            title: {
+              type: String,
+            },
+            description: {
+              type: String,
+            },
+          },
+        ],
+        testcases: [
+          {
+            type: String,
+          },
+        ],
+      },
+    ],
+  },
+  { versionKey: false },
 );
 
 const Course = mongoose.model("Course", CourseScheme);
--- backend/Routes/Course.routes.js
+++ backend/Routes/Course.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { AddCourseStudent, CreateCourse, GetAllCourses } from "../Controllers/Course.controller.js";
+import { AddCourseStudent, AddMcq, CreateCourse, GetAllCourses } from "../Controllers/Course.controller.js";
 import is_authenticated from "../Middleware/isauthenticated.js";
 const CourseRouter = express.Router();
 
 CourseRouter.get("/getall",GetAllCourses);
 CourseRouter.post("/create",CreateCourse);
 CourseRouter.post("/add",AddCourseStudent);
+CourseRouter.post("/addmcq",AddMcq);
 export default CourseRouter;
